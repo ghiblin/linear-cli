affbfb1ed62c7c6c5da8a4fd23f42613ff7404b8
Alessandro Vitali <alessandro.vitali@gmail.com>
2026-04-28 18:19:05 +0200

    feat(graphql): register scalar mappings for DateTime and TimelessDate

--- src/infrastructure/graphql/schema.rs
+++ src/infrastructure/graphql/schema.rs
@@ -1,2 +1,12 @@
 #[cynic::schema("linear")]
 pub mod schema {}
+
+impl cynic::schema::IsScalar<schema::DateTime> for String {
+    type SchemaType = schema::DateTime;
+}
+impl cynic::coercions::CoercesTo<schema::DateTime> for String {}
+
+impl cynic::schema::IsScalar<schema::TimelessDate> for String {
+    type SchemaType = schema::TimelessDate;
+}
+impl cynic::coercions::CoercesTo<schema::TimelessDate> for String {}
